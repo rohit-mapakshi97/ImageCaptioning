80aae7762f50f6406f88eb96d88f49e0e323e0bb
Brooke <brooke.dalton@sjsu.edu>
2022-11-19 18:49:10 -0800

    Save name of training images in a txt file

--- createTestTrainSplit.py
+++ createTestTrainSplit.py
@@ -6,27 +6,28 @@ if __name__ == "__main__":
     test = "res/test/"
     train = "res/train/"
     isExist = os.path.exists(test)
-    
-    if not isExist: 
+
+    if not isExist:
         os.makedirs(test)
     isExist = os.path.exists(train)
-    if not isExist: 
+    if not isExist:
         os.makedirs(train)
 
     files = os.listdir(path)
     train_split = int(len(files) * 0.8)
-    i = 1 
+    i = 1
     while i < len(files)+1:
-        src = path + files[i] 
-        if i < train_split: 
+        src = path + files[i]
+        if i < train_split:
             dst = train + files[i]
             shutil.copy(src, dst)
         else:
             dst = test + files[i]
             shutil.copy(src, dst)
-        i += 1 
+        i += 1
 
-
-                
-
-
+    # Save name of training images in a txt file
+    with open("res/trainImages.txt", "w") as a:
+        for path, subdirs, files in os.walk('res/train'):
+            for filename in files:
+                a.write(str(filename) + os.linesep)
